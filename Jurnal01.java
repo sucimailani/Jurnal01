b1e75c9533f47dc1db7d5b47012512a4169cc0c9
sucimailani <sucimailani20@gmail.com>
2023-09-27 09:33:07 +0700

    Jurnal01

---
+++ Jurnal01.java
@@ -0,0 +1,7 @@
+public class Jurnal01 {
+    public static void main(String[] args) {
+        System.out.println("Nama : Suci Mailanisafitri");
+        System.out.println("Nim : 607062330008");
+        System.out.println("Kelas : D3IF-47-04");
+    }
+}
